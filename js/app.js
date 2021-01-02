705270dc67e12e45ba7822c828a32e201c328d9d
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-01-02 06:19:15 -0600

    Validación para el mensaje en blanco agregada.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
+'use strict';
+
 //--- Variables y selectores ---//
 const formulario = document.querySelector('#formulario');
 const listaTweets = document.querySelector('#lista-tweets');
 let tweets = [];
 
-
 //--- Event listeners ---//
 const eventListeners = () => formulario.addEventListener('submit', agregarTweet);
 eventListeners();
 
-
 //---  Funciones ---//
 function agregarTweet(e) {
 	e.preventDefault();
-	console.log('Agregando tweet...');
+	
+	//obtiene el valor de lo que escribe el usuario en el textarea
+	const tweet = document.querySelector('#tweet').value;
+
+	//validación por si el mensaje viene vacio
+	if (tweet === '') {
+		mostrarError('Un mensaje no puede ir vacio');
+
+		return;//evita que se ejecuten más lineas de código
+	}
+
+	console.log('agregando tweet');
+}
+
+function mostrarError(error) {
+	const mensajeError = document.createElement('p');
+	mensajeError.textContent = error;
+	mensajeError.classList.add('error');
+
+	//insertando el error en el contenido
+	const contenido = document.querySelector('#contenido');
+	contenido.appendChild(mensajeError);
+
+	setTimeout(() => {
+		mensajeError.remove();
+	}, 1500);
 }
